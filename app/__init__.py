1966d1919b6f4f000fece2f722fd7e8f5fdf7e58
gabyjazzmin <gabrielaj.4.f@gmail.com>
2022-07-31 14:41:50 -0500

    fix init.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from crypt import methods
 from dotenv import load_dotenv
 import datetime
 
-#import regex as re
 
 load_dotenv()
 
@@ -50,15 +49,6 @@ def about_gabby():
     return render_template('about_gabby.html', title="MLH Fellow", url=os.getenv("URL"))
 
 
-@app.route('/places/<string:for_person>')
-def visited_map(for_person: str = None):
-    if for_person is not None and for_person.lower() == 'dario':
-        return send_file('templates/maps/dario-places-visited.html')
-
-    if for_person is not None and for_person.lower() == 'gabby':
-        return jsonify({"message": 'map not yet available'}), 200
-
-    return jsonify({"message": "resource not found"}), 404
 
 
 @app.route('/api/timeline_post', methods=['POST'])
